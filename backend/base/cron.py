1019ad7f23d5a9373a0f038f2fca915c52e71a41
Lexander <lexanderthakur3@gmail.com>
2026-09-24 20:48:53 +0530

    done no more learning value left

--- backend/base/cron.py
+++ backend/base/cron.py
@@ -8,7 +8,7 @@ from .models import Ticket
 
 def cron_job():
     cut_off = datetime.now(timezone.utc)-timedelta(minutes=10)
-
+    print("Cron Ran")
     qs = Ticket.objects.filter(status="reserved").filter(
         reserved_at__lt=cut_off
         )
